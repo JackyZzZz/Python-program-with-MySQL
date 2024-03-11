dc70c4c0a431c62949a333e85239749d5a43b091
Jun Xia <xiaj8@uci.edu>
2024-03-11 13:10:08 -0700

    Implemented insertUse function

---
+++ insert_use.py
@@ -0,0 +1,18 @@
+import mysql.connector
+
+def insertUse(projectId, studentUCINetID, machineId, start_date, end_date, conn):
+    """
+    Insert a new use record into the database
+    """
+
+    cursor = conn.cursor()
+    try:
+        query = "INSERT INTO StudentUseMachinesInProject VALUES (%s, %s, %s, %s, %s)"
+        cursor.execute(query, (projectId, studentUCINetID, machineId, start_date, end_date))
+        conn.commit()
+        print('Success')
+    except mysql.connector.Error as error:
+        print('Fail')
+
+    finally:
+        cursor.close()
--- project.py
+++ project.py
@@ -3,6 +3,7 @@ import sys
 from constants import Constants
 import initialization
 import import_data
+import insert_use
 
 def main():
     if len(sys.argv) < 3:
@@ -16,6 +17,18 @@ def main():
         initialization.initialize_database()
         conn = initialization.connect()
         import_data.import_data(folder_name, conn)
+        conn.close()
+
+    if function_name == 'insertUse':
+        # Success: python project.py insertUse 2 aanthony4 1 2024-03-01 2024-03-15
+        # Fail: python project.py insertUse 999 aanthony4 1 2024-03-01 2024-03-15
+        projectId = int(sys.argv[2])
+        studentUCINetID = sys.argv[3]
+        machineId = int(sys.argv[4])
+        start_date = sys.argv[5]
+        end_date = sys.argv[6]
+        conn = initialization.connect()
+        insert_use.insertUse(projectId, studentUCINetID, machineId, start_date, end_date, conn)
         conn.close()
 
 if __name__ == "__main__":
